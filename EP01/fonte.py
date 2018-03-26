b1ba7c6d89d05e01b1fa9db355af56725c091219
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-26 19:28:49 -0300

    Add necessary parenthesis for Python 3 print and change plot axis
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- EP01/fonte.py
+++ EP01/fonte.py
@@ -34,13 +34,13 @@ def calc_vel_and_plot(dataset, coord_x, coord_y, name, plot_color):
     timeline = [float(i) for i in subdataset]
     delta_t = np.sum(timeline)
     velocity = vel(delta_s, delta_t)
-    print("Vel %s: %s m/s") % (name ,velocity)
+    print(("Vel %s: %s m/s") % (name ,velocity))
 
     positions_plot = [eq_horaria(s[0], i, velocity) for i in range(20)] # s[0] comes from constants
-    plt.plot(positions_plot, time_plot, color = plot_color)
+    plt.plot(time_plot, positions_plot, color = plot_color)
 
 # adiciona informacoes ao plot e salva em um arquivo
-def setup_and_save_plot(legend, title, filename, xlabel="Espaco", ylabel="Tempo"):
+def setup_and_save_plot(legend, title, filename, xlabel="Tempo", ylabel="Espaço"):
     plt.legend(legend, loc='upper left')
     plt.title(title)
     plt.xlabel(xlabel)
@@ -56,7 +56,7 @@ calc_vel_and_plot(dataset, 4, [2,5], 'Lucas - Pareado 1', 'blue')
 calc_vel_and_plot(dataset, 8, [2,5], 'Lucas - Pareado 2', 'red')
 calc_vel_and_plot(dataset, 27, [2,8], 'Lucas - Alternado', 'green')
 setup_and_save_plot(['Pareado 1', 'Pareado 2', 'Alternado'],
-    "Exemplo - Lucas",
+    "Lucas",
     "MU-Plot-Lucas.png")
 
 # MUV
