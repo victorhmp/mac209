8f1bf08c020f732c8451d223916a2284f920e2d1
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-27 00:51:26 -0300

    Organize audio files
    Delete StdAudio implementation and create client program
    `generateSound.java` to clean up repository.
    Rename audio files to look better.
    
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

---
+++ EP01/StdAudio/generateSound.java
@@ -0,0 +1,34 @@
+import edu.princeton.cs.algs4.StdAudio;
+
+public class generateSound {
+
+    // create a note (sine wave) of the given frequency (Hz), for the given
+    // duration (seconds) scaled to the given volume (amplitude)
+    private static double[] note(double hz, double duration, double amplitude) {
+        int n = (int) (StdAudio.SAMPLE_RATE * duration);
+        double[] a = new double[n + 1];
+        for (int i = 0; i <= n; i++)
+            a[i] = amplitude * Math.sin(2 * Math.PI * i * hz / StdAudio.SAMPLE_RATE);
+        return a;
+    }
+
+    public static void main(String[] args) {
+        // scale increments
+        int[] steps = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        double time = 1500;
+        for (int i = 0; i < steps.length; i++) {
+            double hz = 440.0 * Math.pow(2, steps[i] / 12.0);
+            StdAudio.play(note(hz, 0.1, 0.5));
+            try {
+                Thread.sleep((int) time);
+                time = time / 1.1;
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        // need to call this in non-interactive stuff so the program doesn't terminate
+        // until all the sound leaves the speaker.
+        StdAudio.close();
+    }    
+}
